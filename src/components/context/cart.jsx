13cc118ab5729824b8f1951b9c1d9863ab61d860
Augustine <mayakiaugustine0@gmail.com>
2024-08-18 07:25:41 +0100

    modal ui implementation

--- src/components/context/cart.jsx
+++ src/components/context/cart.jsx
@@ -4,19 +4,35 @@ export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
+  const [addItem, setAddItem] = useState(false);
+  const [required, setRequired] = useState(false);
+  const [existingItem, setExistingItem] = useState(false);
+  const addedToCART = () => {
+    setAddItem(true);
+    setTimeout(() => setAddItem(false), 2000);
+  };
+  const quantityRequired = () => {
+    setRequired(true);
+    setTimeout(() => setRequired(false), 2000);
+  };
+
+  const itemExist = () => {
+    setExistingItem(true);
+    setTimeout(() => setExistingItem(false), 2000);
+  };
 
   const addToCart = (product, productQuantity) => {
     const isInCart = cartItems.find((item) => item.id === product.id);
-    !isInCart
-      ? productQuantity >= 1
-        ? setCartItems([
-            ...cartItems,
-            { ...product, quantity: productQuantity },
-          ])
-        : console.log(
-            `Please add a valid quantity. The quantity is: ${productQuantity}`
-          )
-      : console.log([...cartItems]);
+    if (!isInCart) {
+      if (productQuantity >= 1) {
+        setCartItems([...cartItems, { ...product, quantity: productQuantity }]);
+        addedToCART();
+      } else {
+        quantityRequired();
+      }
+    } else {
+      itemExist();
+    }
   };
 
   const removeItem = (product) => {
@@ -34,7 +50,15 @@ export const CartProvider = ({ children }) => {
 
   return (
     <CartContext.Provider
-      value={{ cartItems, addToCart, removeItem, clearCart }}
+      value={{
+        cartItems,
+        addToCart,
+        removeItem,
+        clearCart,
+        addItem,
+        required,
+        existingItem,
+      }}
     >
       {children}
     </CartContext.Provider>
